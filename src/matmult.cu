87e440ba8c3277dc288d7e15e5a315287256ab0d
jurc192 <vidmarjur@gmail.com>
2018-09-28 15:27:54 +0200

    Checking array values

--- src/matmult.cu
+++ src/matmult.cu
@@ -27,6 +27,12 @@ int main()
         h_vec_res[i] = 0;
     }
 
+    std::cout << "OUTPUTS:\nh_vec_a\th_vec_res\n";
+    for (int i=0; i<ARR_SIZE; i++) {
+        std::cout << h_vec_a[i] << "\t";
+        std::cout << h_vec_res[i] << "\n";
+    }
+
 
     // Allocate arrays @GPU
     float *d_vec_a;
@@ -42,22 +48,22 @@ int main()
     }
 
 
-    // Transfer data from CPU to GPU
-    if (cudaMemcpy(d_vec_a, h_vec_a, arr_bytes, cudaMemcpyHostToDevice) != cudaSuccess) {
-        std::cout << "Failed at cudaMemcpy H2D\n";
-    }
+    // // Transfer data from CPU to GPU
+    // if (cudaMemcpy(d_vec_a, h_vec_a, arr_bytes, cudaMemcpyHostToDevice) != cudaSuccess) {
+    //     std::cout << "Failed at cudaMemcpy H2D\n";
+    // }
 
-    // Kernel launch
-    square<<<1, ARR_SIZE>>>(d_vec_a, d_vec_res);
+    // // Kernel launch
+    // square<<<1, ARR_SIZE>>>(d_vec_a, d_vec_res);
 
-    // Transfer data from GPU to CPU
-    if (cudaMemcpy(h_vec_res, d_vec_res, arr_bytes, cudaMemcpyDeviceToHost) != cudaSuccess) {
-        std::cout << "Failed at cudaMemcpy D2H\n";
-    }
+    // // Transfer data from GPU to CPU
+    // if (cudaMemcpy(h_vec_res, d_vec_res, arr_bytes, cudaMemcpyDeviceToHost) != cudaSuccess) {
+    //     std::cout << "Failed at cudaMemcpy D2H\n";
+    // }
 
-    // Delete array
-    delete[] h_vec_a;
-    delete[] h_vec_res;
+    // // Delete array
+    // delete[] h_vec_a;
+    // delete[] h_vec_res;
     
     cudaFree(d_vec_a);
     cudaFree(d_vec_res);
